bbc5d1dbc329c4ee9a33cce19e9fe37995f37833
SagaOfAGuy <68156940+SagaOfAGuy@users.noreply.github.com>
2020-12-14 21:58:46 -0500

    Update change_port_rpm.sh

--- change_port_rpm.sh
+++ change_port_rpm.sh
@@ -1,5 +1,8 @@
-# Install SSH on Red-Hat Based system
-sudo dnf install openssh-server 
+# Install openssh-server based on the package manager on Linux system 
+if [ ! -z $(command -v apt) ]; then sudo apt install openssh-server; fi
+if [ ! -z $(command -v dnf) ]; then sudo dnf install openssh-server; fi
+if [ ! -z $(command -v yum) ]; then sudo yum install openssh-server; fi
+if [ ! -z $(command -v pacman) ]; then sudo pacman -S install openssh-server; fi
 
 # Edit sshd config file 
 read -p "Enter a port number: " port
@@ -8,10 +11,13 @@ read -p "Enter a port number: " port
 sudo sed -i "s/Port 524/Port $port/g" /etc/ssh/sshd_config
 echo "Port number successfully changed!"
 
+# Configure changed port in SEmanage if using Red-Hat Linux
+if [ ! -z $(command -v yum) ] || [ ! -z $(command -v dnf) ]; then sudo semanage port -a -t ssh_port_t -p tcp $port
+
 # Restart sshd service 
 sudo service sshd restart 
 
 # Prompt user of success
 echo "SSH Service restarted!"
-
 echo "SSH into your machine by using the -p flag with ssh. Example: ssh -p 128 m1@192.168.1.219"
+
